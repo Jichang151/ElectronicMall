6663a24c2414add7678990d9c854a554de8fba1b
sjc <602205405@qq.com>
2024-06-18 16:26:25 +0800

    update filename and data

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ElectronicMall
 这是一个在线电子产品商城项目
+
--- data/generate-products.js
+++ data/generate-products.js
@@ -9,7 +9,7 @@ const generateProducts = (count) => {
             name: `Product ${i}`,
             description: `This is a description for Product ${i}.`,
             price: getRandomPrice(4000, 20000), // 随机价格
-            image: `../image/img1.avif`, // 示例图像链接
+            image: `../images/img1.avif`, // 示例图像链接
         };
         products.push(product);
     }
@@ -24,4 +24,4 @@ const getRandomPrice = (min, max) => {
 // 生成示例产品数据并保存到 products.json 文件
 const productCount = 100; // 想要生成的产品数量
 const productsData = generateProducts(productCount);
-fs.writeFileSync('products.json', JSON.stringify(productsData, null, 2));
+fs.writeFileSync('data/products.json', JSON.stringify(productsData, null, 2));
